8939b6f394000c06e7f9f18c9d6dde15430aec47
Yusuke Murakami <tropical.inu@gmail.com>
2018-10-31 15:07:39 +0900

    comment_ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,24 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.create(comments_params)
-    if @comment.save
-      redirect_to :root, notice: '投稿完了'
-    else
-      redirect_to :root, alert: '投稿失敗'
-    end
-    # respond_to do |format|
-  	  # 	format.html
-  	  # 	format.json
+    # if @comment.save
+      respond_to do |format|
+        format.html
+        format.json
+      end
+      # redirect_to item_path(params[:item_id]), notice: '投稿完了'
+    # else
+    #   render 'item_path', alert: '投稿失敗'
+    # end
   end
+
+  def edit
+  end
+
+  def update
+
+  end
+
 
   private
   def comments_params
